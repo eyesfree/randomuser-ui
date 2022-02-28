594145e3e68dedbe7d01a222c452ce1c641891ec
eyesfree <kristinavancappelle@protonmail.com>
2022-02-28 17:06:00 +0100

    implement requirements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,114 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { FixedSizeList } from 'react-window';
+import Button from '@mui/material/Button';
+import RefreshIcon from '@mui/icons-material/Refresh';
+
+function getCountriesFromList(users) {
+    let updatedCountries = [];
+    updatedCountries.concat(users.slice());
+    users.map(user => {
+        if (!updatedCountries?.includes(user.location.country)) {
+            updatedCountries.push(user.location.country);
+        }
+    });
+    return updatedCountries;
+}
+
+function getUsersForCountries(users, value) {
+    var filteredEvents = users.filter(item => item.location.country.includes(value));
+    return filteredEvents;
+}
+
+function UsersComponent() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    const [countries, setCountries] = useState([]);
+    const [usersInCountry, setUsersInCountry] = useState([]);
+    const [value, setValue] = useState('');
+    const [refresh, setRefresh] = useState(true);
+
+    useEffect(() => {
+        if (refresh) {
+            console.log('refreshing');
+            setIsLoaded(false);
+            fetch("http://localhost:8080/randomusers/users")
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        setIsLoaded(true);
+                        setItems(result._embedded.users);
+                        setCountries(getCountriesFromList(result._embedded.users));
+                        setUsersInCountry(getUsersForCountries(items, value));
+                        setRefresh(false);
+                        console.log('fetched users count ' + items.length);
+                    },
+                    (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                        setRefresh(false);
+                    }
+                )
+        }
+    }, [refresh])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div style={{ marginLeft: '5%', marginTop: '60px' }}>
+                <Autocomplete
+                    options={countries}
+                    style={{ width: 300 }}
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                        setUsersInCountry(getUsersForCountries(items, newValue));
+                    }}
+                    renderInput={(params) =>
+                        <TextField {...params} label="Countries" variant="outlined" />}
+                />
+              
+                <Button variant="outlined" startIcon={<RefreshIcon />} onClick={() => setRefresh(true)}>
+                    Refresh data
+                </Button>
+                <Box
+                    sx={{ width: '100%', height: 800, maxWidth: 1000, bgcolor: 'background.paper' }}
+                >
+                    <FixedSizeList
+                        height={800}
+                        width={700}
+                        itemSize={46}
+                        itemCount={usersInCountry.length > 0 ? usersInCountry.length : items.length}
+                        overscanCount={20}
+                    >
+                        {({ index }) => {
+                            let item = usersInCountry[index] ? usersInCountry[index] : items[index];
+                            return (
+                                <ListItem key={index} disablePadding>
+                                    <ListItemText primary={`${item.name.title} ${item.name.first} ${item.name.last} ${item.gender} ${item.email} `} />
+                                </ListItem>
+                            );
+                        }}
+                    </FixedSizeList>
+                </Box>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <UsersComponent />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
